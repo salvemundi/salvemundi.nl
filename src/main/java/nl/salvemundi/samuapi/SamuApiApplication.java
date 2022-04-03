e7d385d5667e497d1d530eab1f87077ed92702eb
Quint <quint.legius@iqfx.nl>
2022-04-03 15:43:53 +0200

    first commit

---
+++ src/main/java/nl/salvemundi/samuapi/SamuApiApplication.java
@@ -0,0 +1,13 @@
+package nl.salvemundi.samuapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamuApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamuApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/salvemundi/samuapi/SamuApiApplicationTests.java
@@ -0,0 +1,33 @@
+package nl.salvemundi.samuapi;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@SpringBootTest
+class SamuApiApplicationTests {
+//	@InjectMocks
+//	private EntityServiceHier entityService;
+//
+//	@Mock
+//	private EntityServiceRepository entityRepository;
+//
+//	@BeforeEach
+//	public void setUp() {
+//		entityService = new EntityServiceHier(entityRepository);
+//		MockitoAnnotations.openMocks(entityService);
+//	}
+//
+//	@Test
+//	public void test() {
+//		assertThat(true).isEqualTo(true);
+//	}
+}
