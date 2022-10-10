a25e9c5b5b4a0a0f541a90ebabac805678bdd905
Sanne <sanne2610h@gmail.com>
2022-10-10 08:20:13 +0200

    Added footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,14 +1,55 @@
 <template>
-    <div class="footer">
-        <b-container class="bv-example-row">
-            <b-row>
-                <b-col>
-                     
-                </b-col>
-                <b-col>2 of 3</b-col>
-                <b-col>3 of 3</b-col>
-            </b-row>
-        </b-container>
+    <div class="footer d-flex justify-content-center">
+        <b-row class="mx-5">
+            <b-col class="my-5">
+                <h5>INFORMATIE</h5>
+                <a href="https://goo.gl/maps/dnv849aVPd8DyXqa8" target="https://goo.gl/maps/dnv849aVPd8DyXqa8">
+                    Rachelsmolen 1 <br>
+                    5612 MA Eindhoven <br>
+                </a>
+                KVK nr. 70280606 <br>
+                <a href="/responsible-disclosure">Responsible Disclosure</a><br>
+                <a href="../assets/files/privacy.pdf" download>Download privacyvoorwaarden</a><br>
+                <a href="../assets/files/Huisregelement.pdf" download>Download huisregelement</a><br>
+                <a href="../assets/files/Statuten-vereniging-Salve-Mundi.pdf" download>Download statuten</a>
+            </b-col>
+            <b-col class="my-5">
+                <h5>CONTACT</h5>
+                <a  href="mailto:info@salvemundi.nl">info@salvemundi.nl</a><br>
+                <a href="tel:+31 6 24827777">+31 6 24827777</a><br>
+                <a href="https://api.whatsapp.com/send/?phone=%2B31624827777&text&app_absent=0" target="_blank">
+                <i class="fab fa-whatsapp"></i> Whatsapp</a><br>
+            </b-col>
+            <b-col class="my-5">
+                <h5>COMMISSIES</h5>
+                Bestuur <br>
+                Feestcommissie <br>
+                Mediacommissie <br>
+                Introcommissie <br>
+                Kascommissie <br>
+                ICT-commissie <br>
+                Kamcommissie <br>
+                Activiteitencommissie <br>
+                Studiecommissie <br>
+                Reiscommissie <br>
+            </b-col>
+            <b-col class="my-5">
+                <h5>SOCIAL MEDIA</h5>
+                <i class="fa fa-instagram"></i> <a href="https://www.instagram.com/sv.salvemundi/" target="_blank">Instagram</a><br>
+                <i class="fa fa-facebook"></i> <a href="https://nl-nl.facebook.com/sv.salvemundi/" target="_blank">Facebook</a><br>
+                <i class="fa fa-linkedin"></i> <a href="https://www.linkedin.com/company/salve-mundi/" target="_blank">LinkedIn</a><br>
+
+            </b-col>
+            <b-col class="col-12 line">
+                <b-row>
+                    <b-col class="col-md-12 col-sm-12 col-xs-12 text-center mt-3">
+                        <p class="center">Copyright &copy; 2022 &nbsp;
+                            <a href="#">Salve Mundi</a>&nbsp;alle rechten voorbehouden. <a href="https://github.com/salvemundi/salvemundi.nl-laravel" target="_blank"> &nbsp;<i class="fab fa-github"></i> Source code</a>
+                        </p>
+                    </b-col>
+                </b-row>
+            </b-col>
+        </b-row>
     </div>
 </template>
 
@@ -16,5 +57,18 @@
 .footer{
     background-color: #663366;
     color: white;
+}
+.footer a{
+    text-decoration: none;
+    color: white;
+}
+
+.footer a:hover{
+    color: plum;
+}
+
+
+.line{
+    border-top: 2px solid plum;
 }
 </style>
