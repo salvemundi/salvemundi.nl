7eaee273f26c2689990d56e82184f5d85d6858d2
Quint Legius <quint.legius@gmail.com>
2022-11-07 18:44:37 +0100

    added comment

--- pages/auth/index.vue
+++ pages/auth/index.vue
@@ -5,8 +5,9 @@
 
 <script lang="ts">
 // eslint-disable-next-line camelcase
-import jwt_decode from 'jwt-decode'
+// import jwt_decode from 'jwt-decode'
 export default {
+  // DO NOT RUN THIS CODE. Good for testing, vulnerable af
   // mounted () {
   //   const decodedBearer = jwt_decode(this.$auth.$storage.getUniversal('_token.aad'))
   //   this.$auth.$storage.setUniversal('jwt_decoded', decodedBearer)
